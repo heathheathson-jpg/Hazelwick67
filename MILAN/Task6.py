36a28d87e2f34fb8e3839f1feafaae7f8eba0576
heathheathson-jpg <heathheathson@gmail.com>
2026-09-22 08:52:03 +0000

    22/09/26

---
+++ MILAN/Task6.py
@@ -0,0 +1,19 @@
+nam1 = input("Enter your name: ")
+nam2 = input("Enter your name: ")
+nam3 = input("Enter your name: ")
+nam4 = input("Enter your name: ")
+mat1 = input("Enter a material: ")
+mat2 = input("Enter a material: ")
+mat3 = input("Enter a material: ")
+
+print(f"There once were three little pigs. The first
+pig was called {nam1}, the second was
+called {nam2} and the third was called
+{nam3}. The first house was made from
+[material 1]. The second house was made
+from [Material 2]. The third house was made
+from [Material 3]. The big bad wolf was
+called [Name 4]. The big bad wolf blew
+down the first two houses. He could not
+destroy the third house made from bricks.
+The pigs lived happily ever after. ​")
